feca3c28d4ccbbb4af18b068a1746374c1f24aef
zine22 <kamiljamil15@gmail.com>
2023-06-15 06:11:20 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 # include <stdio.h>
+/**
+ * main - entry point
+ *
+ *description for main : print this text using puts function
+ *  
+ * return always 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle\n");
-return (0);
+  puts("\"Programming is like building a multiligual puzzle\n");
+  return (0);
 }
