31305346fee759a37d51fbbbd430f706e8e324c2
zine22 <kamiljamil15@gmail.com>
2023-06-15 11:15:45 -0700

    simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: with scannf you can print
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+scannf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
