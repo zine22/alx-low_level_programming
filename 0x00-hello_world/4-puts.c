c8e0fb3666a1dc0b55694da21bc4ac6b9e43c7f0
zine22 <kamiljamil15@gmail.com>
2023-06-15 06:43:04 -0700

    yesbetty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
+
 /**
- * main - entry point
- *
- *description for main: print this text using puts function  
- * return always 0 (success)
+ *main - Entry point
+ *description for main: print this text using puts function
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multiligual puzzle\n");
