ba276a6ae0653b999f4d4cc6cc1d94721866f76c
zine22 <kamiljamil15@gmail.com>
2023-06-15 12:25:31 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,13 @@
 */
 
 int main(void)
+{
 int a;
 char b;
 long int c;
 long long int d;
 float e;
-{
+
 printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
