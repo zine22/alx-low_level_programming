d8c9f9768c001db0b9e9736586180c4e91b0e1c6
zine22 <kamiljamil15@gmail.com>
2023-06-16 08:05:30 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (success)
+*
+* Description: print all numbers of base 16 with func putchar
+*/
+
+int main(void)
+{
+char i = 48;
+char a = 97;
+while (i <= 57 && a <= 102)
+{
+putchar(i);
+putchar(a);
+i++;
+a++;
+}
+return (0);
+}
