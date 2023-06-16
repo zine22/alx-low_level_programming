aded41baa03bb17bcd447ada21893b37edef2e00
zine22 <kamiljamil15@gmail.com>
2023-06-15 19:56:11 -0700

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
 *
 *Description: with scannf you can print
 *
-*Return: Always 0 (Success)
+*Return: 1 (Success)
 */
 
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
