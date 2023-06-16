4e62e6a22e25b6f16f659c49eab9dbdf332edc91
zine22 <kamiljamil15@gmail.com>
2023-06-15 19:53:01 -0700

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
