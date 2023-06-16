33295634e463d94019b249fbada3c742aad68a49
zine22 <kamiljamil15@gmail.com>
2023-06-16 09:33:05 -0700

    num

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,11 @@
 #include<time.h>
 
 /**
- * Main - Entry point
- *
- * Description: assign a random number to var n
- *
- *Initial n:Declaration of n
- *
- * Return always zero (success)
+* Main - Entry point
+*
+* Return: Always 0 (success)
+*
+* Description: print if number sorted is positive or negative using if;else and printf function
 */
 
 int main(void)
@@ -17,11 +15,19 @@ int n;
 
 srand(time(o));
 n = rand() - RAND_MAX / 2;
+
+
 if (n > 0)
+{
 printf("the number is positive");
+}
 else if (n < 0)
+{
 printf("the number is negative");
+}
 else
+{
 printf("the number is zero");
+}
 return (0);
 }
