5756040f3923c283e8b8f9e17b4ff8c47c576c19
zine22 <kamiljamil15@gmail.com>
2023-06-16 07:10:09 -0700

    from0to9using ASCII

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - Enttry point
+*
+* Return: always 0 (success)
+*
+* Description: print numbers from 0 to 9 with func putchar
+*/
+
+int main(void)
+{
+char i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+return (0);
+}
