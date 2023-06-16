9d25dfdc26d2ca7f9311eedaa7d768179b11ec1c
zine22 <kamiljamil15@gmail.com>
2023-06-16 07:44:07 -0700

    z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: is always 0 (success)
+*
+* Description: print alphabets in reverse with func putchar
+*/
+
+int main(void)
+{
+char i = 'z';
+while (i => 'a')
+{
+putchar(i);
+i++;
+}
+return (0);
+}
