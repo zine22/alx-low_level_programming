c888396f9b210df321dfa36b0cfcf23fdc64ce33
zine22 <kamiljamil15@gmail.com>
2023-06-16 05:55:01 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<tsdio.h>
+
+/**
+* main - entry point
+*
+*Return: always 0 'success)
+*
+*Description: to pring all alphabet we used a loop and putchar func
+*/
+
+int main(void)
+{
+
+char i = 'a';
+
+while (i <= 'z')
+{
+putchar(i);
+i++
+}
+return (0);
+}
