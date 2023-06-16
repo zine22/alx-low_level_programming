28c6e699200402658e34b026eb930e4004ec6976
zine22 <kamiljamil15@gmail.com>
2023-06-16 07:29:57 -0700

    0 to 9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0 (success)
+*
+* Description: print numbers from 0 to 9 with func putchar
+*/
+
+int main(void)
+{
+char i = 48;
+while (i <= 57)
+{
+putchar(i\n);
+i++;
+}
+return (0);
+}
