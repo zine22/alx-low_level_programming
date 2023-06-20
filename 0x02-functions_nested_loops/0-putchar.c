d5630c24b70d5f304719727f98544191eedb0daf
zine22 <kamiljamil15@gmail.com>
2023-06-20 05:43:03 -0700

    -putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* Main - Entry point
+*
+* Description: print _printchar by using putchar function
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+putchar("_putchar");
+return (0);
+}
