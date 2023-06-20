afad2ac55b8e97a791ed5306f90dfd918c252750
zine22 <kamiljamil15@gmail.com>
2023-06-20 10:19:49 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0 (success)
+*
+* print_alphabet - by using _putchar function alphabets a to z
+*/
+
+int main(void)
+{
+void print_alphabet(void)
+{
+int a = 97
+while (a <= 122)
+{
+_putchar(a);
+a++;
+}
+-putchar("\n");
+}
+return (0);
+}
