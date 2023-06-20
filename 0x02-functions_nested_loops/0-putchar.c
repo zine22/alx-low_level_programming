d6088e7359e511a751141c130f68357099d12d10
zine22 <kamiljamil15@gmail.com>
2023-06-20 08:21:58 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,20 @@
 #include "main.h"
 
 /**
-* Main - Entry point
-*
-* Description: print _putchar by using putchar prototype
-*
-* Return: always 0 (success)
+ * main - Entry point
+ *
+ * Description: Prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
 */
 
 int main(void)
 {
-putchar("_");
-putchar("p");
-putchar("u");
-putchar("t");
-putchar("c");
-putchar("h");
-putchat("a");
-putchar("r");
+char str[] = "_putchar";
+int i;
 
+ for (i = 0; i < 8; i++)
+_putchar(str[i]);
+_putchar("\n");
 return (0);
 }
