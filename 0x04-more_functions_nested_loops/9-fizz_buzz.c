d1ef2eb5a22c65a2989e879e09d23dcab7ea7a6c
zine22 <kamiljamil15@gmail.com>
2023-06-22 17:53:13 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
 /**
+* main - Entry point
 *
-*
-*
-*
-*
+* Return: always 0
 */
 
 int main(void)
 {
+int n = 100;
+int i;
+
+for (i = 1; i <= n; i++)
+{
+while (n % 3)
+{
+printf("Fizz ");
+}
+while (n % 5)
+{
+printf("Buzz ");
+}
+while (n % 3 && n % 5)
+{
+printf("Fizz Buzz ");
+}
+printf("%d ", i);
+}
+return (0);
+}
