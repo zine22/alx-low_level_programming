88394a9c3f133ae60d1cf5903d6e6e2151c998f3
zine22 <kamiljamil15@gmail.com>
2023-06-26 11:15:40 -0700

    rev print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - print string but in reverse
+*
+* @s: character to be printed
+*
+*/
+
+void print_rev(char *s)
+{
+while (*s != '\0')
+{
+_putchar(*s);
+s--
+}
+_putchar('\n');
+}
