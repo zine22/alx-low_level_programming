ddb69cd228b3c1204810808967835798b5cc90b9
zine22 <kamiljamil15@gmail.com>
2023-07-05 18:31:30 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print the length of a string
+ *
+ * @s: pointer to the string
+ *
+ * Return: always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+-putchar('\n');
+return (0);
+}
