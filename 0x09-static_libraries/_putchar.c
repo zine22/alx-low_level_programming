b9c36752a46c983af49f265e0dba828ce7628475
zine22 <kamiljamil15@gmail.com>
2023-07-08 16:54:17 -0700

    libmy

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,3 +1,24 @@
+#include<stdio.h>
 #include "main.h"
 
-int _putchar(char c);
+/**                                                                                                                                                  
+ * main - Entry point                                                                                                                                
+ *                                                                                                                                                   
+ * Description: Prints _putchar using putchar prototype                                                                                              
+ *                                                                                                                                                   
+ * Return: Always 0 (success)                                                                                                                        
+*/
+
+int main(void)
+{
+char str[] = "_putchar";
+int i = 0;
+
+while (i < 8)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
