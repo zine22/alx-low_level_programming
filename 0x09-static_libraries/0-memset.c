81d18fce785e80e8739b115e24d8a0cbdd694be1
zine22 <kamiljamil15@gmail.com>
2023-07-08 17:23:45 -0700

    libmy

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**                                                                                                                                                  
+* main - function to check if c is uppercase                                                                                                     
+*                                                                                                                                                    
+* @c: number to be checked                                                                                                                           
+* Return: 0                                                                                                           
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+return (0);
+}
