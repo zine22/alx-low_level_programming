bb18bb611a1dfaee4cf5bb2ed91c51c0b43994e1
zine22 <kamiljamil15@gmail.com>
2023-07-08 17:03:00 -0700

    libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**                                                                                                                                                  
+* _abs - computes the absolute value of an int                                                                                                       
+*                                                                                                                                                    
+* Return: Always 0 (success)                                                                                                                         
+*                                                                                                                                                    
+* @n: the integer to be checked                                                                                                                      
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * (-1);
+}
+else
+{
+return (n);
+}
+return (0);
+}
