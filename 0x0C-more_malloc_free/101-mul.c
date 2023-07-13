672118ced6431f7dee6f278b03aacf589fa4b082
zine22 <kamiljamil15@gmail.com>
2023-07-13 16:59:54 -0700

    multi

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - entry point
+*  multiply two numbers
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+float num1, num2;
+int mul, i, j;
+
+for (i = 0; i < num1; i++)
+;
+for (j = 0; j < num2; j++)
+;
+_putchar(num1 * num2);
+
+return (0);
+
+}
