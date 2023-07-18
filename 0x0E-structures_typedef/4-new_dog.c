69e5af6d6e94a5a0032aae1544c551f54db21b8c
zine22 <kamiljamil15@gmail.com>
2023-07-17 18:07:44 -0700

    free

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - free the dogs
+*
+* @d: dog member
+*
+* Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+if (d->age)
+free(d->age);
+
+free(d);
+}
+
+}
