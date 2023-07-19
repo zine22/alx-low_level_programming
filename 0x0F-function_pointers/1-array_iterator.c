f31832c81a5883f194a1fbcc8fbcf5725796447f
zine22 <kamiljamil15@gmail.com>
2023-07-19 14:03:45 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* void array_iterator - a func that execute func
+*
+* @array: an array
+* @size: the size of array
+* @action: func pointer
+*
+* Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if ( array && size && action)
+action(*array++);
+}
