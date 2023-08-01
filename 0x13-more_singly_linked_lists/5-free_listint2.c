8dc212f58db6aa2b32edaf227c28778dc1211d17
zine22 <kamiljamil15@gmail.com>
2023-08-01 13:31:02 -0700

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,8 +12,8 @@ void free_listint2(listint_t **head)
 listint_t *temp;
 
 if (!head)
-return (NULL);
+return;
 
-for (; *head; *head = *head->next, free(temp))
+for (; *head; *head = (*head)->next, free(temp))
 temp = *head;
 }
