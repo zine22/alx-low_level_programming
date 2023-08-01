05c4e8194ca27b0c1888fd593af9fc7c8102545f
zine22 <kamiljamil15@gmail.com>
2023-08-01 12:27:32 -0700

    print all

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listsint - prints all elements
+*
+* @h: pointer
+*
+* Return: i
+*/
+
+size_t print_listint(const listint_t *h)
+{
+int i;
+
+for (i = 0; h; h = h->next, i++)
+_putchar(h->n);
+
+return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this dir is for more linked lists tasts
