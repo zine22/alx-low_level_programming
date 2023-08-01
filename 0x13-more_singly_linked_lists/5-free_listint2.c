f0545b16b3acdf9451321884b09bf1792e942626
zine22 <kamiljamil15@gmail.com>
2023-08-01 13:28:54 -0700

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free list
+*
+* @head: head of list
+*
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+
+if (!head)
+return (NULL);
+
+for (; *head; *head = *head->next, free(temp))
+temp = *head;
+}
