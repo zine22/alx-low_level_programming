1df289dcecc58b4937fb12d7a4ac8db566292713
zine22 <kamiljamil15@gmail.com>
2023-08-01 12:46:18 -0700

    lengh

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* listint_len - print lengh
+*
+* @h: pointer
+*
+* Return: i
+*/
+
+size_t listint_len(const listint_t *h)
+{
+int i;
+
+for (i = 0; h; h = h->next, i++)
+return (i);
+}
