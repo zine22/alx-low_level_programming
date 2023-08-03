5fdb8c248fedcfbf3bca422cb55435d56db0ee7b
zine22 <kamiljamil15@gmail.com>
2023-08-03 15:32:00 -0700

    clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* clear_bit - clear bits
+*
+* @n: number
+* @index: index
+*
+* Return: 1 or -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= 64)
+return (-1);
+
+*n &= ~(1 << index);
+return (1);
+}
