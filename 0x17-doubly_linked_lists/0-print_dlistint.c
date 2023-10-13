fdfbcf47f0abd0f0f9407df8a324c9c2925d2cba
zine22 <kamiljamil15@gmail.com>
2023-10-12 17:20:04 -0700

    prt list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists."
+
+/**
+* print_dlistint - Func to print all elements of list
+*
+* @h: argument
+*
+* Return: the number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *temp;
+size_t i;
+
+temp = h;
+i = 0;
+
+while (temp != NULL)
+{
+i += 1;
+printf("%d\n", temp->n);
+temp = temp->next;
+}
+return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this dir is for doubly linked lists
