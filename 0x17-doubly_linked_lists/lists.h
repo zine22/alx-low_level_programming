d5d980768561f9ecb120493ec097ecbf7982f412
zine22 <kamiljamil15@gmail.com>
2023-10-12 17:29:03 -0700

    print

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib>
+#include <string.h>
+
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+}dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -14,11 +22,5 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-typedef struct dlistint_s
-{
-int n;
-struct dlistint_s *prev;
-struct dlistint_s *next;
-}dlistint_t;
 
 #endif
